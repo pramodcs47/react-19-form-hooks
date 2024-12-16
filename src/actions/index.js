510621d97d8042fb08b147e547aef4f191b511e0
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-12-16 14:26:27 +0530

    (fix) missed a fullstop in the message.

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,7 @@ export async function addUserToRole(prevState, formData) {
         await new Promise((resolve) => {
             setTimeout(resolve, 2000);
         });
-        return `The user ${userName} exists for the role ${role} already. Can't add it again`;
+        return `The user ${userName} exists for the role ${role} already. Can't add it again.`;
     } else {
         addUser(userName, role);
         return `Added the user ${userName} to the role ${role} successfully.`;
