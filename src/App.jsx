3f589bb79bc0ede7b224949a7cf5781446818794
Pramod Krishna <pramodcs47@zohomail.in>
2026-05-06 19:29:31 +0530

    Changed the rendering component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { BasicUsage } from "./basic-usage";
+import { DataUsage } from "./data-usage";
 import { Pitfall } from "./pitfall";
 
 function App() {
   return (
     <div className="p-4 flex justify-around">
-     <Pitfall />
+     <DataUsage />
     </div>
    
   );
